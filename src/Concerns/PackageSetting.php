b25b5d760df24e00f55b7b9409ef1423c5c61201
Denny <dennykuo@gmail.com>
2021-07-02 13:11:22 +0800

    dev

--- src/Concerns/PackageSetting.php
+++ src/Concerns/PackageSetting.php
@@ -5,7 +5,7 @@ namespace Dennykuo\AdminView\Concerns;
 trait PackageSetting
 {
     private static $viewNamespace = 'admin';
-    private static $assetsLinkSrcPath = 'vendor/dennykuo/laravel-admin-view/assets';
+    private static $assetsLinkSrcPath = 'vendor/room701/laravel-admin-view/assets';
     private static $publishAssetsPath = 'vendor/laravel-admin-view';
     private static $publishConfigName = 'admin-view';
 }
