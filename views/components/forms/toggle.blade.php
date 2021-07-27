53c1fffc44739d98b825cebfcd86add0481d80df
Denny <dennykuo@gmail.com>
2021-07-27 15:06:08 +0800

    dev

--- views/components/forms/toggle.blade.php
+++ views/components/forms/toggle.blade.php
@@ -2,7 +2,7 @@
   'name' => null,
   'label' => null,
   'value' => '1',
-  'ceheked' => true,
+  'ceheked' => null, // value, null, false (不做任何預先選擇)
   'wrapperClass' => '',
   'inputClass' => '',
 ])
@@ -21,8 +21,7 @@
 
   <div>
     <label for="toggle-{{ $name }}" class="form-toggle inline-block cursor-pointer relative {{ $inputClass }}">
-      {{-- <input id="{{ $name }}" name="{{ $name }}" value={{ $value }} type="checkbox" class="hidden" {!! $attributesHTML !!}> --}}
-      {!! Form::checkbox($name, $value, $ceheked, $attributes) !!}
+      {!! Form::checkbox($name, $value, $checked == $value ? true : null, $attributes) !!}
       <div class="bar w-12 h-7 bg-gray-200 rounded-full shadow-inner border border-gray-100 transition duration-300"></div>
       <div class="dot absolute w-5 h-5 bg-white rounded-full shadow-md left-1 top-1 transition duration-300"></div>
     </label>
