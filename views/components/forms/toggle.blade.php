a2588a931dc3a69047933f65e2e625dbfdac3d30
Denny <dennykuo@gmail.com>
2021-07-27 14:56:02 +0800

    dev

--- views/components/forms/toggle.blade.php
+++ views/components/forms/toggle.blade.php
@@ -18,10 +18,12 @@
     <label>{{ $label }}</label>
   @endif
 
-  <label for="toggle-{{ $name }}" class="form-toggle inline-block cursor-pointer relative {{ $inputClass }}">
-    {{-- <input id="{{ $name }}" name="{{ $name }}" value={{ $value }} type="checkbox" class="hidden" {!! $attributesHTML !!}> --}}
-    {!! Form::checkbox($name, $value, null, $attributes) !!}
-    <div class="bar w-12 h-7 bg-gray-200 rounded-full shadow-inner border border-gray-100 transition duration-300"></div>
-    <div class="dot absolute w-5 h-5 bg-white rounded-full shadow-md left-1 top-1 transition duration-300"></div>
-  </label>
+  <div>
+    <label for="toggle-{{ $name }}" class="form-toggle inline-block cursor-pointer relative {{ $inputClass }}">
+      {{-- <input id="{{ $name }}" name="{{ $name }}" value={{ $value }} type="checkbox" class="hidden" {!! $attributesHTML !!}> --}}
+      {!! Form::checkbox($name, $value, null, $attributes) !!}
+      <div class="bar w-12 h-7 bg-gray-200 rounded-full shadow-inner border border-gray-100 transition duration-300"></div>
+      <div class="dot absolute w-5 h-5 bg-white rounded-full shadow-md left-1 top-1 transition duration-300"></div>
+    </label>
+  </div>
 </div>
