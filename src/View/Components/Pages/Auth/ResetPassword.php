fd23748c3639d133d0281a6ae3a07282be0709de
Denny <dennykuo@gmail.com>
2021-07-28 17:18:56 +0800

    dev

---
+++ src/View/Components/Pages/Auth/ResetPassword.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Dennykuo\AdminFerry\View\Components\Pages\Auth;
+
+use Illuminate\View\Component;
+
+class ResetPassword extends Component
+{
+    public $submitUrl;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($submitUrl)
+    {
+        $this->submitUrl = $submitUrl;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('admin-ferry::auth.reset-password');
+    }
+}
--- views/auth/@section.blade.php
+++ views/auth/@section.blade.php
@@ -1,16 +1,9 @@
 @extends('admin-ferry::layouts.auth')
 
 @section('content')
-  <div class="lg:flex items-center justify-center">
+  <div class="lg:flex items-center justify-center h-screen bg-gradient-to-b from-indigo-50 to-gray-100">
 
-    {{-- heading set --}}
-    <div class="hidden lg:flex flex-1 h-screen lg:w-1/2 items-center justify-center bg-gradient-to-b from-indigo-50 to-gray-100">
-      <div class="max-w-xs transform duration-200 hover:scale-110 cursor-pointer">
-        <h1 class="text-8xl text-indigo-600 font-extralight tracking-wider">{{ $heading }}</h1>
-      </div>
-    </div>
-
-    <div class="w-full lg:w-1/2">
+    <div class="w-full lg:w-1/2 xl:w-2/5 bg-white pt-8 pb-4 mb-24">
 
       {{-- logo set --}}
       <div class="flex justify-center lg:justify-start items-center bg-indigo-100 lg:bg-white py-10 px-10 lg:max-w-lg mx-auto">
@@ -37,7 +30,7 @@
       </div>
 
       {{-- form set --}}
-      <div class="mt-24 lg:mt-14 mb-14 px-10 lg:max-w-lg mx-auto">
+      <div class="mt-20 lg:mt-12 mb-12 px-10 lg:max-w-lg mx-auto">
         {{-- <h2 class="text-center text-4xl text-indigo-900 font-semibold lg:text-left xl:text-5xl xl:text-bold">登入</h2> --}}
 
         @if ($errors->any())
@@ -48,38 +41,8 @@
           <x-admin-ferry::alert.notify :message="session('status')" wrapper-class="mb-10" />
         @endif
 
-        <form action="{{ $submitUrl }}" method="POST">
+        @yield('form')
 
-          @csrf
-
-          <input name="remember" type="hidden" value="1">
-
-          <div>
-            <div class="text-sm font-semibold text-gray-500">Email</div>
-            <input name="email" type="email" value="{{ old('email') }}" class="w-full text-lg px-0 py-1 border-0 border-b border-gray-300 focus:outline-none focus:border-indigo-500" required>
-          </div>
-
-          <div class="mt-8">
-            <div class="flex justify-between items-center">
-              <div class="text-sm font-semibold text-gray-500">密碼</div>
-            </div>
-            <input name="password" type="password" class="w-full text-lg px-0 py-1 border-0 border-b border-gray-300 focus:outline-none focus:border-indigo-500" required>
-          </div>
-
-          @if ($passwordResetUrl)
-            <div class="mt-8">
-              <div class="flex justify-end">
-                <a href="{{ $passwordResetUrl }}" class="text-xs font-semibold text-indigo-600 hover:text-indigo-800 cursor-pointer">忘記密碼</a>
-              </div>
-            </div>
-          @endif
-
-          <div class="mt-10">
-            <button class="bg-indigo-500 text-gray-100 p-4 w-full rounded-full font-semibold focus:outline-none focus:shadow-outline hover:bg-indigo-600 shadow-md">
-              登入
-            </button>
-          </div>
-        </form>
         {{-- <div class="mt-12 text-sm font-semibold text-gray-700 text-center">
           Don't have an account ? <a class="cursor-pointer text-indigo-600 hover:text-indigo-800">Sign up</a>
         </div> --}}
