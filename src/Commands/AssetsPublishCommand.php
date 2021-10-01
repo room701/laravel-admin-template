200637506a3dbe0e48227d363b3459d57f9e0ea6
Denny <dennykuo@gmail.com>
2021-10-01 11:50:32 +0800

    dev

--- src/Commands/AssetsPublishCommand.php
+++ src/Commands/AssetsPublishCommand.php
@@ -16,14 +16,21 @@ class AssetsPublishCommand extends Command
 
     public function handle()
     {
-        $prependDirName = \Str::before(static::$publishAssetsPath, '/'); // ex: vendor
-        if(! File::isDirectory(public_path($prependDirName))){
-            File::makeDirectory(public_path($prependDirName, 0755, true, true));
+        $assetsLinkSrcPath = base_path(static::$assetsLinkSrcPath);
+        $publishAssetsPath = base_path(static::$publishAssetsPath);
+
+        // 創建目標目錄的上一層目錄 (ln 指令需要)
+        $prependDir = \Str::beforeLast($publishAssetsPath, '/'); // ex: public/vendor
+
+        if (! File::isDirectory($prependDir)) {
+            File::makeDirectory($prependDir);
         }
 
-        $source = base_path(static::$assetsLinkSrcPath);
-        $to = 'public/' . static::$publishAssetsPath;
-        system("ln -s {$source} {$to}");
+        // 先刪除目錄 (防止舊目錄存在，導致路徑錯誤)
+        File::deleteDirectory(base_path($publishAssetsPath));
+
+        // 連結目錄
+        system("ln -s {$assetsLinkSrcPath} {$publishAssetsPath}"); // ln -s {來源完整路徑} {目標路徑(以當前位置為基準)}
 
         $this->info('laravel-admin-ferry 的 assets 設定完成');
     }
--- src/Concerns/PackageSetting.php
+++ src/Concerns/PackageSetting.php
@@ -5,7 +5,8 @@ namespace Dennykuo\AdminFerry\Concerns;
 trait PackageSetting
 {
     private static $viewNamespace = 'admin-ferry';
-    private static $assetsLinkSrcPath = 'vendor/dennykuo/laravel-admin-ferry/assets';
-    private static $publishAssetsPath = 'vendor/laravel-admin-ferry';
     private static $publishConfigName = 'admin-ferry';
+
+    private static $assetsLinkSrcPath = 'vendor/dennykuo/laravel-admin-ferry/assets';
+    private static $publishAssetsPath = 'public/vendor/laravel-admin-ferry';
 }
