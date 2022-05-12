615d368c2bc4b17885a1e2a57c616568785923ea
Denny <dennykuo@gmail.com>
2022-05-12 17:14:12 +0800

    dev

--- src/AdminFerryServiceProvider.php
+++ src/AdminFerryServiceProvider.php
@@ -58,9 +58,9 @@ class AdminFerryServiceProvider extends ServiceProvider
     protected function bootForConsole()
     {
         // Artisan commands
-        // $this->commands([
-        //     Commands\AssetsPublishCommand::class,
-        // ]);
+        $this->commands([
+            Commands\AssetsPublishCommand::class,
+        ]);
 
         // Publish assets
         // 需要改成另外的資料夾位置，為使用者自己修改的資源了
