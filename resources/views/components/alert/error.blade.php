2206949efa6844f2a73ae9ce5e483e200e24628a
Denny <dennykuo@gmail.com>
2022-05-15 10:21:26 +0800

    dev

--- resources/views/components/alert/error.blade.php
+++ resources/views/components/alert/error.blade.php
@@ -1,33 +1,23 @@
 @props([
   'errors',
   'wrapperClass' => '',
-  'closeBtnClass' => '',
 ])
 
-<div class="rounded-md flex items-center jusitfy-between px-5 py-2 border border-red-400 text-red-600 {{ $wrapperClass }}">
-  <div class="w-full flex items-center">
+<div class="flex items-center bg-red-50 border-t-4 border-red-600 px-4 py-4 text-red-600 font-semibold shadow-sm {{ $wrapperClass }}">
+  {{-- <svg class="h-8 w-8 text-red-600 mr-6 cursor-pointer" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z">
+  </svg> --}}
 
-    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="w-5 h-5 mr-6" viewBox="0 0 1792 1792">
-      <path d="M1024 1375v-190q0-14-9.5-23.5t-22.5-9.5h-192q-13 0-22.5 9.5t-9.5 23.5v190q0 14 9.5 23.5t22.5 9.5h192q13 0 22.5-9.5t9.5-23.5zm-2-374l18-459q0-12-10-19-13-11-24-11h-220q-11 0-24 11-10 7-10 21l17 457q0 10 10 16.5t24 6.5h185q14 0 23.5-6.5t10.5-16.5zm-14-934l768 1408q35 63-2 126-17 29-46.5 46t-63.5 17h-1536q-34 0-63.5-17t-46.5-46q-37-63-2-126l768-1408q17-31 47-49t65-18 65 18 47 49z">
-      </path>
-    </svg>
+  <i class="fa-solid fa-circle-exclamation mr-4 fa-xl"></i>
 
-    @if (is_string($errors))
-      {{ $errors }}
-    @else
-      <ul class="list-disc list-inside">
-        @foreach ($errors->all() as $error)
-          <li>{{ $error }}</li>
-        @endforeach
-      </ul>
-    @endif
+  @if (is_string($errors))
+    {{ $errors }}
+  @else
+    <ul>
+      @foreach ($errors->all() as $error)
+        <li class="leading-7">{{ $error }}</li>
+      @endforeach
+    </ul>
+  @endif
 
-  </div>
-
-  <button type="button"
-          class="-mr-1 flex-shrink-0 flex rounded-md sm:-mr-2 {{ $closeBtnClass }}"
-          onclick="this.parentElement.remove();"
-    >
-    <i class="fa-solid fa-xmark fa-xl"></i>
-  </button>
 </div>
