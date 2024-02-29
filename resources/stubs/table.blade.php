c9e9fb45b37c49ed4ab49a764f6c5c94cdb17986
Denny <dennykuo@gmail.com>
2024-02-29 10:58:52 +0800

    dev

--- resources/stubs/table.blade.php
+++ resources/stubs/table.blade.php
@@ -13,37 +13,41 @@ breadcrumb:
     <x-admin-ferry::button href="#">新增</x-admin-ferry::button>
   </div>
 
-  <x-admin-ferry::tables.data-table is-responsive>
-    <tr>
-      <th>編號</th>
-      <th class="w-1/2">名稱</th>
-      <th class="w-1/5">內容</th>
-      <th>標籤</th>
-      <th>開啟</th>
-      <th data-tool>動作</th>
-    </tr>
+  {{-- @if ($data->isNotEmpty()) --}}
+    <x-admin-ferry::tables.data-table is-responsive>
+      <tr>
+        <th>編號</th>
+        <th class="w-1/2">名稱</th>
+        <th class="w-1/5">內容</th>
+        <th>標籤</th>
+        <th>開啟</th>
+        <th data-tool>動作</th>
+      </tr>
 
-    <tr>
-      <td data-label="編號">
-        {{--  --}}
-      </td>
-      <td data-label="名稱">
-        {{--  --}}
-      </td>
-      <td data-label="內容">
-        {{--  --}}
-      </td>
-      <td data-label="標籤">
-        {{--  --}}
-      </td>
-      <td data-label="開啟">
-        {{--  --}}
-      </td>
-      <x-admin-ferry::tables.td-tool>
-        <x-admin-ferry::button href="#">修改</x-admin-ferry::button>
-        <x-admin-ferry::button tag="button" color="red">刪除</x-admin-ferry::button>
-      </x-admin-ferry::tables.td-tool>
-    </tr>
-  </x-admin-ferry::tables.data-table>
+      <tr>
+        <td data-label="編號">
+          {{--  --}}
+        </td>
+        <td data-label="名稱">
+          {{--  --}}
+        </td>
+        <td data-label="內容">
+          {{--  --}}
+        </td>
+        <td data-label="標籤">
+          {{--  --}}
+        </td>
+        <td data-label="開啟">
+          {{--  --}}
+        </td>
+        <x-admin-ferry::tables.td-tool>
+          <x-admin-ferry::button href="#">修改</x-admin-ferry::button>
+          <x-admin-ferry::button tag="button" color="red">刪除</x-admin-ferry::button>
+        </x-admin-ferry::tables.td-tool>
+      </tr>
+    </x-admin-ferry::tables.data-table>
+  {{-- @else --}}
+    <x-admin-ferry:x-admin-ferry::tables.no-data />
+  {{-- @endif --}}
 
 </x-admin-ferry::section>
