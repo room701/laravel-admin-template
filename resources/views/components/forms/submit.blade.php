d8035aecd553b85a432182430996646b6102c303
Denny <dennykuo@gmail.com>
2024-03-29 12:25:01 +0800

    dev

--- resources/views/components/forms/submit.blade.php
+++ resources/views/components/forms/submit.blade.php
@@ -8,5 +8,5 @@
 <div class="w-100 h-px"></div>{{-- Just for space padding --}}
 
 <div class="ferry-form-submit {{ $wrapperClass }}">
-  <button type="{{ $type }}" class="ferry-form-submit-button {{ $inputClass }}">{{ $text }}</button>
+  <button type="{{ $type }}" class="ferry-form-submit-button {{ $inputClass }}" {{ $attributes }}>{{ $text }}</button>
 </div>
