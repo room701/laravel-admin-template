fd739920cb5ec886a71b28a3105115cbd679289a
Denny <dennykuo@gmail.com>
2025-02-22 00:33:35 +0800

    dev

--- resources/js/components/modal.js
+++ resources/js/components/modal.js
@@ -1,28 +1,52 @@
 (function() {
     window.admin.modal = {
         open: (id) => {
-            let modal = getModal(id);
-            modal.classList.remove('hidden');
+            let elements = getElements(id);
+            elements.modal.classList.add('scale-100');
+            elements.modal.classList.remove('scale-0');
+            elements.panel.classList.add('!opacity-100', '!translate-y-0', '!sm:scale-100');
 
             document.body.classList.add('overflow-hidden');
         },
 
         close: (id) => {
-            let modal = getModal(id);
-            modal.classList.add('hidden');
+            let elements = getElements(id);
+            elements.modal.classList.add('scale-0');
+            elements.modal.classList.remove('scale-100');
+            elements.panel.classList.remove('!opacity-100', '!translate-y-0', '!sm:scale-100');
 
             document.body.classList.remove('overflow-hidden');
         },
 
         toggle: (id) => {
-            let modal = getModal(id);
-            modal.classList.toggle('hidden');
+            let elements = getElements(id);
+            elements.modal.classList.toggle('scale-100');
+            elements.modal.classList.toggle('scale-0');
+            elements.panel.classList.toggle('!opacity-100');
+            elements.panel.classList.toggle('!translate-y-0');
+            elements.panel.classList.toggle('!sm:scale-100');
 
             document.body.classList.toggle('overflow-hidden');
         }
     };
 
+    function getElements(id) {
+        let modal = getModal(id);
+        let panel = getPanel(modal);
+
+        console.log(panel)
+
+        return {
+            modal: modal,
+            panel: panel
+        };
+    }
+
     function getModal(id) {
         return document.getElementById(id);
+    }
+
+    function getPanel(modal) {
+        return modal.querySelector('[data-modal-panel]');
     }
 })();
--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -24,80 +24,80 @@
     To: "opacity-0"
 -->
 
-<section id="{{ $id }}" class="{{ $isShow ? 'block' : 'hidden' }} fixed inset-0 z-[9999]">
+{{-- Modal --}}
+<section id="{{ $id }}" class="{{ $isShow ? 'scale-100 is-show' : 'scale-0' }} fixed inset-0 z-[9999] flex items-center justify-center p-4 text-center sm:p-0 !m-0">
 
-  {{-- 背景遮罩 --}}
-  <div class="fixed inset-0 bg-gray-200/70 bg-opacity-70 transition-opacity duration-200"></div>
+  {{-- Background overlay --}}
+  <div class="fixed inset-0 bg-gray-600/40 transition-opacity duration-200"></div>
 
-  {{-- Modal --}}
-  <div class="fixed inset-0 w-screen overflow-y-auto">
-    <div class="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
-      <!--
-        Modal panel, show/hide based on modal state.
+  <!--
+    Modal panel, show/hide based on modal state.
 
-        Entering: "ease-out duration-300"
-          From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-          To: "opacity-100 translate-y-0 sm:scale-100"
-        Leaving: "ease-in duration-200"
-          From: "opacity-100 translate-y-0 sm:scale-100"
-          To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-      -->
-      <div class="relative w-full sm:max-w-lg transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8">
+    Entering: "ease-out duration-300"
+      From: "opacity-30 translate-y-12 sm:scale-80"
+      To: "opacity-100 translate-y-0 sm:scale-100"
+    Leaving: "ease-in duration-200"
+      From: "opacity-100 translate-y-0 sm:scale-100"
+      To: "opacity-30 translate-y-12 sm:scale-80"
+  -->
+  {{-- Modal panel --}}
+  <div data-modal-panel class="relative w-full sm:max-w-lg overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all duration-500 sm:my-8 opacity-30 translate-y-12 sm:scale-80">
 
-        {{-- 關閉按鈕 --}}
-        @if ($hasCloseButton)
-          <div class="absolute top-0 right-0 pt-4 pr-4">
-            <button type="button" class="js-modal-close w-6 h-6">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="oc se"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path></svg>
-            </button>
-          </div>
-        @endif
+    {{-- 關閉按鈕 --}}
+    @if ($hasCloseButton)
+      <div class="absolute inset-y-0-0 right-0 pt-5 sm:pt-6 pr-5">
+        <button type="button" class="js-modal-close w-5 h-5">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="oc se"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path></svg>
+        </button>
+      </div>
+    @endif
 
-        {{-- 內容 --}}
-        <div class="bg-white px-4 py-5 sm:px-6 sm:py-6">
-          <div class="w-full sm:flex sm:items-start">
-            {{-- <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"> --}}
-            {{--   <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true"> --}}
-            {{--     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" /> --}}
-            {{--   </svg> --}}
-            {{-- </div> --}}
-            <div class="w-full mt-3 mr-4 sm:ml-4 sm:mt-0">
-              {{-- 標題 --}}
-              @if ($title)
-                <h3 class="text-lg font-semibold leading-6 text-gray-900">
-                  {{ $title }}
-                </h3>
-              @endif
+    <!-- Modal content -->
+    <div class="bg-white px-4 py-5 sm:px-5 sm:py-6">
+      <div class="w-full sm:flex sm:items-start">
+        {{-- <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"> --}}
+        {{--   <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true"> --}}
+        {{--     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" /> --}}
+        {{--   </svg> --}}
+        {{-- </div> --}}
+        <div class="w-full mt-3 mr-5 sm:mt-0">
+          {{-- 標題 --}}
+          @if ($title)
+            <h3 class="text-lg font-semibold leading-6 text-gray-900 pr-5">
+              {{ $title }}
+            </h3>
+          @endif
 
-              {{-- 內容 --}}
-              @if ($slot->isNotEmpty())
-                <div @class(['mt-5' => ! empty($title), 'text-gray-500'])>
-                  @if ($contentAllowHtml)
-                    {!! $slot !!}
-                  @else
-                    {{ $slot }}
-                  @endif
-                </div>
+          {{-- 內容 --}}
+          @if ($slot->isNotEmpty())
+            <div @class(['mt-5' => ! empty($title), 'text-gray-500'])>
+              @if ($contentAllowHtml)
+                {!! $slot !!}
+              @else
+                {{ $slot }}
               @endif
             </div>
-          </div>
+          @endif
         </div>
-
-        {{-- 動作按鈕 --}}
-        @if ($hasFooter)
-          <div class="bg-gray-100/90 px-4 py-4 sm:flex sm:flex-row-reverse sm:px-6">
-            <button type="button" class="js-modal-callback inline-flex w-full sm:w-auto justify-center rounded-md px-1.5 py-2.5 font-semibold text-white shadow-sm !bg-red-400 hover:!bg-red-500 sm:ml-3">{{ $confirmBtnText }}</button>
-
-            <button type="button" class="js-modal-close inline-flex w-full sm:w-auto mt-3 sm:!mt-0 justify-center rounded-md !bg-white px-1.5 py-2.5 font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-              {{ $cancelBtnText }}</button>
-          </div>
-        @endif
-
       </div>
     </div>
+    <!-- /Modal content -->
+
+    @if ($hasFooter)
+      {{-- Modal footer --}}
+      <div class="bg-gray-100/90 px-4 py-4 sm:flex sm:flex-row-reverse sm:px-6 text-sm">
+        <button type="button" class="js-modal-callback inline-flex w-full sm:w-auto justify-center rounded-md px-4 py-2.5 text-white shadow-sm !bg-red-400 hover:!bg-red-500 sm:ml-3">{{ $confirmBtnText }}</button>
+
+        <button type="button" class="js-modal-close inline-flex w-full sm:w-auto mt-3 sm:!mt-0 justify-center rounded-md !bg-white px-4 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+          {{ $cancelBtnText }}</button>
+      </div>
+      {{-- /Modal footer --}}
+    @endif
   </div>
+  {{-- /Modal panel --}}
 
 </section>
+{{-- /Modal wrapper --}}
 
 <script>
   (function() {
